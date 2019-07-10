2f998f2254bd0d5777af39ecae05b94e5f02e51d
akshay85131 <akshay85131@gmail.com>
2019-07-10 15:25:03 +0530

    commented all passport

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const passportLocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new Schema({
-    username: String,
-    password: String
-});
+// const UserSchema = new Schema({
+//     username: String,
+//     password: String
+// });
 
-UserSchema.plugin(passportLocalMongoose);
+// UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('user', UserSchema);
+// module.exports = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -6,27 +6,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const tripRoutes = require('./routes/route')
 const app = express()
-const passport = require('passport')
-const cookieSession = require('cookie-session')
-const LocalStrategy = require('passport-local').Strategy
+// const passport = require('passport')
+// const cookieSession = require('cookie-session')
+// const LocalStrategy = require('passport-local').Strategy
 const PORT = 3000
-// import mongoose from 'mongoose'
-// import postNewTrip from './control/helpers'
-// require('./models/config')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieSession)({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-})
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(cookieSession)({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: false
+// })
+// app.use(passport.initialize())
+// app.use(passport.session())
 
-const user =require('./models/user')
-passport.use(new LocalStrategy(user.authenticate()))
-passport.serializeUser(user.serializeUser())
-passport.deserializeUser(user.deserializeUser())
+// const user =require('./models/user')
+// passport.use(new LocalStrategy(user.authenticate()))
+// passport.serializeUser(user.serializeUser())
+// passport.deserializeUser(user.deserializeUser())
 // app.get('/', (req, res) => {
 //   res.json('hello')
 // })
