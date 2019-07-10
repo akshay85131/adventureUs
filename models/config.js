9ba55df1ec9c31e5ca47261bf8c554b704a1523a
akshay85131 <akshay85131@gmail.com>
2019-07-11 01:58:27 +0530

    mongo

--- models/config.js
+++ models/config.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose')
 const trips = require('./tripSchema')
 mongoose.set('debug', true)
-const connection = mongoose.connect('mongodb:mongodb+srv://akshay4362:akshay@4362@cluster0-tra13.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+const connection = mongoose.connect('mongodb+srv://akshay4362:akshay@4362@cluster0-tra13.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
 //  const connection =mongoose.connect ('mongodb://admin:Akshay004362@localhost:27017/trip', { useNewUrlParser: true }, (err) => {
   if (!err) {
     console.log('connection successfull')
