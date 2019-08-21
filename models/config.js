07b6a5f982ba132b88f87adf42141138096cfffc
akshay85131 <akshay85131@gmail.com>
2019-08-22 00:10:57 +0530

    heroku uri

--- models/config.js
+++ models/config.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const trips = require('./tripSchema')
 mongoose.set('debug', true)
-const connection = mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }, (err) => {
-// const connection = mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err) => {
+// const connection = mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }, (err) => {
+const connection = mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err) => {
   if (!err) {
     console.log('connection successfull')
   } else {
